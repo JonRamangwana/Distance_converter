c4b2fcfa961a420c16868b1ce9457a4bd198b8d8
JonRamangwana <jonramangwana@outlook.com>
2022-10-15 13:27:39 +0200

    Add files via upload

---
+++ km_to_miles_converter.py
@@ -0,0 +1,11 @@
+#Convert miles to kilometers
+
+miles1 = float(input("Enter miles: "))
+km1 = miles1 * 1.6
+
+def distance_converter():
+    print(miles1)
+    print(input("= " + str(km1) + " km"))
+
+distance_converter()
+
